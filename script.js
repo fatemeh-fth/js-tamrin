edc12760e23575925417535d95777c84644f8b73
fatemeh-fth <fatemeh.fathi350@gmail.com>
2022-04-09 11:11:22 +0430

    convert

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var username="your name is :rasta"
 // alert(username)
 
-var age=prompt("سن خود را وارد نمایید:" , 19)
-alert(`your age is ${age}`)
+// var age=prompt("سن خود را وارد نمایید:" , 19)
+// alert(`your age is ${age}`)
+
+// /* ----------------part 8 */
+// var userAge=prompt ("inter your name:")
+// var userNameAge=Number(userAge)
+// alert(typeof userNameAge)
+
+
